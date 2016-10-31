513ec722e60e5e9ec641614938e41f9b0fdc52e3
hsetiawan <hendset.ti04@gmail.com>
2016-10-31 10:39:38 +0700

    upload tugas backed end developer

---
+++ README.txt
@@ -0,0 +1,17 @@
+ReadMe
+
+1. project drupal ada di folder "drupalkuy" .
+	*include ultimate cron (pengaturan cron job, agar cron running pada jam 12 siang)
+	*include oneAll social login -sudah link dengan fb dan google
+	*aktif rest api
+
+2. file sql dengan nama "drupalkuy31102016.sql" .
+3. nama database "drupalkuy" dengan prefix "kuy_" .
+4. file test CRUD REST APi ada di folder "testrest" file configurasi nya di "jscript/main.js" (silahkan di atur url nya bila berbeda settingany).
+
+notes :
+untuk tugas REST API , saya masih belum bisa mengirimkan/meng-ubah file,saya hanya bisa sampai merubah deskripsinya saja
+
+
+sekian
+Terima Kasih
---
+++ drupalkuy/sites/default/files/php/twig/1719e2a8_menu-local-action.html.twig_21f062ddd68300ca05a49a85748885f92988113aaa3b3106faeec969894a8c75/154237f85463021217c54088852b6221f3dd486b0f0f18b27c21a4076f83573a.php
@@ -0,0 +1,79 @@
+<?php
+
+/* core/themes/classy/templates/navigation/menu-local-action.html.twig */
+class __TwigTemplate_a243d6fd81b8b05f0e1966e68be89413cb329caae3a297fd66e8e2af2c40dcf8 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "<li";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo ">";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["link"]) ? $context["link"] : null), "html", null, true));
+        echo "</li>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/navigation/menu-local-action.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a single local action link.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: HTML attributes for the wrapper element.*/
+/*  * - link: A rendered link element.*/
+/*  **/
+/*  * @see template_preprocess_menu_local_action()*/
+/*  *//* */
+/* #}*/
+/* <li{{ attributes }}>{{ link }}</li>*/
+/* */
